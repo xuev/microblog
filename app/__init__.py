a581848ef656d547a563caa8f5b05e500becbe16
xuev <xuewei918@gmail.com>
2017-03-01 19:02:03 +0800

    Init

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author:  xuev (xuewei918@gmail.com)
+@Project: microblog
+@File: __init__.py
+@Version: 0.01
+@License: MIT Licence
+@Create Time: 2017/3/1 下午6:59
+@Description: 
+"""
+
+
